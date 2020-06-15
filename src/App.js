cd724077a8f2d9e215c59231b90c8833bc61c2cf
Charles Brands <charles.brands@gmail.com>
2020-06-15 20:15:03 +0200

    finished app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Button from "./components/Button";
+
 import "./styles.css";
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = increment => {
+    setCount(count + increment);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Button increment={1} onClickFunction={incrementCount} />
+      <Button increment={10} onClickFunction={incrementCount} />
+      <Button increment={100} onClickFunction={incrementCount} />
+      <Button increment={1000} onClickFunction={incrementCount} />
+      <span>{count}</span>
     </div>
   );
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
